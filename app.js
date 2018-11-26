51bc0e43857e22e316c10ac0608836cbd2cb7c50
Alex Choong <choong_alex@hotmail.com>
2018-11-26 22:05:30 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const Movie = require('./Movie');
+const Lyricsdata = require('./lyricsdata');
 //const path = require('path'); //---heroku---
 const cors = require('cors');
 const apikey = '1435c2f8';
+const googlesearchkey = 'AIzaSyDP6H0pxT1_s6NJ9mXuGQB6ffL3cSWOViM';
+const cs = '011366554791527084890:4kptwuxhhja';
+
+const youtubeapikey = 'AIzaSyDiz0Ihpf002Yb5MzX4K6LeD_xYk0h38n8';
 
 const port = process.env.PORT || 2000;
 
@@ -18,43 +22,42 @@ app.use(function(req, res, next) {
   next();
 });
 
-//localhost:5000/getmovie?title=MovieTitle
-app.get('/getmovie', (req, res) => {
+//localhost:2000/getlyrics?artist=ArtistName&title=Title
+app.get('/getlyrics', (req, res) => {
   const title = req.query.title;
-  const querystr = `http://www.omdbapi.com/?t=${title}&apikey=${apikey}`;
+  const artist = req.query.artist;
 
   axios
-    .get(querystr)
-    .then(response => {
-      const movie = new Movie({
-        title: response.data.Title,
-        year: response.data.Year,
-        genre: response.data.Genre,
-        actors: response.data.Actors,
-        plot: response.data.Plot,
-        poster: response.data.Poster
-      });
-      if (!movie.title) {
-        res.status(200).json('Not found');
-        return;
-      }
-      movie
-        .save()
-        .then(response => {
-          res.status(200).json(response);
-        })
-        .catch(error => {
-          res.status(400).json(error);
+    .all([
+      axios.get(`https://api.lyrics.ovh/v1/${artist}/${title}`),
+      axios.get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${title} ${artist}&maxResults=25&key=${youtubeapikey}`
+      )
+    ])
+    .then(
+      axios.spread((lyricsres, youtuberes) => {
+        const videoid = youtuberes.data.items[0].id.videoId;
+        const youtubeurl = `www.youtube.com/watch?v=${videoid}`;
+        const lyrics = new Lyricsdata({
+          title: title,
+          artist: artist,
+          lyrics: lyricsres.data.lyrics,
+          link: youtubeurl
         });
+        return lyrics.save();
+      })
+    )
+    .then(response => {
+      res.status(200).json(response);
     })
     .catch(error => {
       res.status(400).json(error);
     });
 });
 
-//localhost:5000/getallmovies
-app.get('/getallmovies', (req, res) => {
-  Movie.find({})
+//localhost:2000/getalllyrics
+app.get('/getalllyrics', (req, res) => {
+  Lyricsdata.find({})
     .then(response => {
       res.status(200).send(response);
     })
@@ -63,9 +66,9 @@ app.get('/getallmovies', (req, res) => {
     });
 });
 
-//localhost:5000/deletemovie?title=MovieTitle
-app.get('/deletemovie', (req, res) => {
-  Movie.deleteMany({ title: req.query.title })
+//localhost:2000/deletelyrics?title=LyricsTitle
+app.get('/deletelyrics', (req, res) => {
+  Lyricsdata.deleteMany({ title: req.query.title })
     .then(response => {
       res.status(200).json(response);
     })
